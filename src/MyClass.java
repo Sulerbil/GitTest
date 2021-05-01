a3618f5fafabb7a049b0d0abed9aa631e27366c7
Sulerbil <79145928+Sulerbil@users.noreply.github.com>
2021-05-01 18:59:01 +0100

    Update MyClass.java

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,5 +3,7 @@ public class MyClass {
         System.out.println("Ilk commit");
         System.out.println("Ikinci commit");
         System.out.println("ucuncu commit");
+        System.out.println("Sena'nin commit'i");
+        
     }
 }
